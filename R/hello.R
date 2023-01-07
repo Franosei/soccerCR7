b4dd2d1863664a1fba43b04899de0dd9520b4990
Francis Osei <92299789+Franosei@users.noreply.github.com>
2023-01-07 13:50:57 +0100

    first commit

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,29 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+library(leagueCR7)
+library(tidyverse)
+require(foreign)
+require(nnet)
+
+
+#' Title
+#'
+#' @param dataset Football leagues dataset: default of leagueCR7::leagueCR7
+#' @param condition  The name of the league
+#'
+#' @return
+#' @export
+#'
+#' @examples
+trainCR7 <- function(dataset = leagueCR7, condition){
+  leagueCR7 <- leagueCR7 |>
+    dplyr::filter({{condition}})|>
+    dplyr::select(-c(is_cup,league_name))
+  leagueCR7$target <- as.factor(leagueCR7$target)
+  leagueCR7$home_team_name <- as.factor(leagueCR7$home_team_name)
+  levels(leagueCR7$target) <- c("Away","Draw","Home")
+  train <- multinom(target ~., data = leagueCR7)
+  train
 }
+
