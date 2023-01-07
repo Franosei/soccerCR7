68553161324fe021a3cacd452203b0f199c2b536
Francis Osei <92299789+Franosei@users.noreply.github.com>
2023-01-07 22:01:42 +0100

    second commit

--- R/hello.R
+++ R/hello.R
@@ -1,14 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
+
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
@@ -19,23 +9,36 @@ require(foreign)
 require(nnet)
 
 
-#' Title
+#' Footbal League Matches Prediction
 #'
 #' @param dataset Football leagues dataset: default of leagueCR7::leagueCR7
-#' @param condition  The name of the league
+#' @param testdata  The new dataset to predict: default of leagueCR7::leagueCR7
+#' @param league  The name of the league
 #'
-#' @return
+#' @return A predicted Probabilities of League Matches is save as result.xlxs
 #' @export
 #'
-#' @examples
-trainCR7 <- function(dataset = leagueCR7, condition){
-  leagueCR7 <- leagueCR7 |>
-    dplyr::filter({{condition}})|>
-    dplyr::select(-c(is_cup,league_name))
-  leagueCR7$target <- as.factor(leagueCR7$target)
-  leagueCR7$home_team_name <- as.factor(leagueCR7$home_team_name)
-  levels(leagueCR7$target) <- c("Away","Draw","Home")
-  train <- multinom(target ~., data = leagueCR7)
-  train
+#' @examples trainCR7()
+#' @examples trainCR7(leagueCR7, leagueCR7, league_name == "Eliteserien")
+#' @examples trainCR7(leagueCR7, leagueCR7, league_name == "Ligue 1")
+
+
+
+trainCR7 <- function(dataset = leagueCR7::leagueCR7,testdata = leagueCR7::leagueCR7, league = (league_name == "Bundesliga")){
+    leagueCR7 <- leagueCR7::leagueCR7 |>
+      dplyr::filter({{league}})|>
+      dplyr::select(-c(is_cup,league_name))
+    leagueCR7$target <- as.factor(leagueCR7$target)
+    leagueCR7$home_team_name <- as.factor(leagueCR7$home_team_name)
+    levels(leagueCR7$target) <- c("Away","Draw","Home")
+    testdata <- testdata |>
+      dplyr::filter({{league}})|>
+      dplyr::select(-c(is_cup,league_name))
+    testdata$target <- as.factor(testdata$target)
+    testdata$home_team_name <- as.factor(testdata$home_team_name)
+    levels(testdata$target) <- c("Away","Draw","Home")
+    test <- nnet::multinom(target ~., data = testdata)
+    pp <- round(predict(test, newdata = testdata, "probs"),2)
+    write.table(pp, file = "result.txt", sep = "  ")
 }
 
---
+++ README.md
@@ -0,0 +1,68 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# soccerCR7
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of soccerCR7 is to predict football league matches across the
+world. The final prediction is save as result.txt with the the
+probability of Away team win, Draw mach and Home team win
+
+## Installation
+
+You can install the development version of soccerCR7 from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+remotes::install_github("Franosei/soccerCR7")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+remotes::install_github("Franosei/Football_dataset_package/leagueCR7")
+#> Skipping install of 'leagueCR7' from a github remote, the SHA1 (5a8e7b1a) has not changed since last install.
+#>   Use `force = TRUE` to force installation
+library(leagueCR7)
+library(soccerCR7)
+```
+
+``` r
+trainCR7(leagueCR7, leagueCR7, league_name == "Eliteserien")
+#> # weights:  84 (54 variable)
+#> initial  value 272.455848 
+#> iter  10 value 216.486192
+#> iter  20 value 198.464259
+#> iter  30 value 196.696212
+#> iter  40 value 196.568082
+#> iter  50 value 196.529341
+#> iter  60 value 196.505971
+#> iter  70 value 196.504715
+#> iter  70 value 196.504714
+#> iter  70 value 196.504714
+#> final  value 196.504714 
+#> converged
+```
+
+``` r
+trainCR7(leagueCR7, leagueCR7, league_name == "Ligue 1")
+#> # weights:  318 (210 variable)
+#> initial  value 1329.320869 
+#> iter  10 value 1225.388887
+#> iter  20 value 1114.506010
+#> iter  30 value 1102.444851
+#> iter  40 value 1100.534796
+#> iter  50 value 1100.444479
+#> iter  60 value 1100.423454
+#> iter  70 value 1100.394272
+#> iter  80 value 1100.391715
+#> iter  90 value 1100.377724
+#> iter 100 value 1100.374559
+#> final  value 1100.374559 
+#> stopped after 100 iterations
+```
